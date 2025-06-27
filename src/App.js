94b67b2c2165e59374c16339c0ead9395c336a55
Kassim <kassimrooney@gmail.com>
2025-06-27 08:48:25 +0300

    removed scripts and imported through App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Register from './components/Register';
+import SearchBar from './components/SearchBar';
+import EventForm from './components/EventForm';
+import EventDetail from './components/EventDetail';
+import Dashboard from './components/Dashboard';
+import CommentSection from './components/CommentSection';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Login />
+      <Profile />
+      <Register />
+      <SearchBar />
+      <EventForm />
+      <EventDetail />
+      <Dashboard />
+      <CommentSection />
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+
