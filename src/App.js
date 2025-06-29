0a4e3c15c4c697986b259f19bf2d9d25052dba6e
Kassim <kassimrooney@gmail.com>
2025-06-29 15:01:38 +0300

    add  a default route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
